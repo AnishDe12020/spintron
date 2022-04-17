58014110242fa8079276713b32f9153601f97e5d
AnishDe12020 <63192115+AnishDe12020@users.noreply.github.com>
2022-04-17 17:58:29 +0530

    chore(example): add more comments explaining all code

--- _example/main.go
+++ _example/main.go
@@ -12,25 +12,25 @@ func main() {
 		Text: "Loading",
 	})
 
-	s.Start() // Start the spinner
-	time.Sleep(2 * time.Second)
-	s.Text = "New character set 👀"             // Change the text
-	s.UpdateCharSet(spintron.CharSets["pong"]) // Update spinner to use a different character set
-	time.Sleep(2 * time.Second)
-	s.Text = "Made the spinner faster :)"
-	s.UpdateSpeed(time.Duration(50) * time.Millisecond) // Update spinner to use a different speed
-	time.Sleep(2 * time.Second)
-	s.Text = "We have gone back to the default character set and speed and changed the color of the spinner"
-	s.UpdateCharSet(spintron.CharSets["dots2"])
-	s.Delay = time.Duration(100) * time.Millisecond
-	err := s.Color("red") // Update spinner to use a different color
+	s.Start()                                                                                                // Start the spinner
+	time.Sleep(2 * time.Second)                                                                              // Simulate a long running process
+	s.Text = "New character set 👀"                                                                           // Change the text
+	s.UpdateCharSet(spintron.CharSets["pong"])                                                               // Update spinner to use a different character set
+	time.Sleep(2 * time.Second)                                                                              // Simulate a long running process
+	s.Text = "Made the spinner faster :)"                                                                    // Updates spinner text
+	s.UpdateSpeed(time.Duration(50) * time.Millisecond)                                                      // Update spinner to use a different speed
+	time.Sleep(2 * time.Second)                                                                              // Simulate a long running process
+	s.Text = "We have gone back to the default character set and speed and changed the color of the spinner" // Updates spinner text
+	s.UpdateCharSet(spintron.CharSets["dots2"])                                                              // Update spinner to use a different character set
+	s.Delay = time.Duration(100) * time.Millisecond                                                          // Update spinner to use a different delay
+	err := s.Color("red")                                                                                    // Update spinner to use a different color
 	if err != nil {
-		s.Fail("Uh oh! Something went wrong!")
+		s.Fail("Uh oh! Something went wrong!") // Stops the spinner and persists it with an error sign and message
 	}
-	time.Sleep(4 * time.Second)
-	s.Text = "The spinner has been reversed"
-	s.Reverse()
-	time.Sleep(2 * time.Second)
-	s.Unicorn("Hope you liked the demo!")
+	time.Sleep(4 * time.Second)              // Simulate a long running process
+	s.Text = "The spinner has been reversed" // Updates spinner text
+	s.Reverse()                              // Reverses the spinner character set
+	time.Sleep(2 * time.Second)              // Simulate a long running process
+	s.Unicorn("Hope you liked the demo!")    // Stops the spinner and persists it with a unicorn sign and message
 
 }
